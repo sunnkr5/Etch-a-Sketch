2b42ff6533cb4f78ae30baffb70edca9dfc94f9f
sunnkr5 <sunnkr5@gmail.com>
2025-02-27 18:29:32 +0530

    created the grid

---
+++ sketch.js
@@ -0,0 +1,22 @@
+const container = document.querySelector("#container");
+
+
+function setGrid(squareNos) {
+    let s1 = squareNos * squareNos;
+    let hw = (480 - (squareNos * 10)) / squareNos;
+    for (let index = 0; index < s1; index++) {
+        const content = document.createElement("div");
+        content.setAttribute("style", "background: orange; margin: 5px");
+        content.style.width = hw + 'px';
+        content.style.height = hw + 'px';
+        container.appendChild(content);
+    }
+}
+console.log(setGrid(4));
+
+        
+
+
+
+
+
