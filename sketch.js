f5daa9803cee3675f0d95eb83b951f4520d14d7d
sunnkr5 <sunnkr5@gmail.com>
2025-02-28 12:48:52 +0530

    made changes to enhance ui

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const container = document.querySelector("#container");
 
 function setGrid(squareNos) {
     let s1 = squareNos * squareNos;
-    let hw = (480 - (squareNos * 10)) / squareNos;
+    let hw = (960 - (squareNos * 10)) / squareNos;
     for (let index = 0; index < s1; index++) {
         const content = document.createElement("div");
         content.setAttribute("style", "background: orange; margin: 5px");
