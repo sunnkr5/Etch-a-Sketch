233938d2edea45b477d3047a226d6131da911783
sunnkr5 <sunnkr5@gmail.com>
2025-02-28 17:00:18 +0530

    added classlist for content

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setGrid(squareNos) {
     let hw = (960 - (squareNos * 10)) / squareNos;
     for (let index = 0; index < s1; index++) {
         const content = document.createElement("div");
-        content.setAttribute("style", "background: orange; margin: 5px");
+        content.classList.add('content-grids');
         content.style.width = hw + 'px';
         content.style.height = hw + 'px';
         container.appendChild(content);
